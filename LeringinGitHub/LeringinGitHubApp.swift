424582e7b3db2bd224ac718640503febdb7a3c72
Daniel <daniel.vik1@icloud.com>
2023-01-04 16:03:44 +0100

    Initial Commit

---
+++ LeringinGitHub/LeringinGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeringinGitHubApp.swift
+//  LeringinGitHub
+//
+//  Created by Daniel vik on 04/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct LeringinGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
